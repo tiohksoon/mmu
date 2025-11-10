4b35654464ada6f61ed2e9949f459face33bcfc4
tiohksoon <tiohksoon@gmail.com>
2025-11-10 05:04:00 +0000

    clipspy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import clips 
+import logging
 
 st.subheader("User Input")
 name = st.text_input("Enter your name")
@@ -7,3 +9,21 @@ if name:
 
 fruit = st.selectbox("Choose a fruit", ["Apple", "Banana", "Orange"])
 st.write(f"You chose: {fruit}")
+
+
+
+# Setup working environment
+logging.basicConfig(level=15,format='%(message)s')
+    
+env = clips.Environment()
+router = clips.LoggingRouter()
+env.add_router(router)
+
+env.build("""(defrule room
+                (cold)
+             =>
+             (printout t "Best plant is ivy!" crlf))""")
+
+# Display rules
+# env.eval("(list-defrules)")  
+env.eval("(ppdefrule room)")
