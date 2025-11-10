813f7bf05592da73c88e743adb3a7dce8e4e9c19
tiohksoon <tiohksoon@gmail.com>
2025-11-10 05:29:42 +0000

    Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,6 @@ import streamlit as st
 import clips 
 import logging
 
-st.subheader("User Input")
-name = st.text_input("Enter your name")
-if name:
-    st.write(f"Hello, {name}!")
-
-fruit = st.selectbox("Choose a fruit", ["Apple", "Banana", "Orange"])
-st.write(f"You chose: {fruit}")
-
-
-
 # Setup working environment
 logging.basicConfig(level=15,format='%(message)s')
     
@@ -19,12 +9,20 @@ env = clips.Environment()
 router = clips.LoggingRouter()
 env.add_router(router)
 
-env.build("""(defrule room
-                (cold)
-             =>
-             (printout t "Best plant is ivy!" crlf))""")
+# input
+name = st.text_input("Enter your name")
 
-# Display rules
-# env.eval("(list-defrules)")  
-# env.eval("(ppdefrule room)")
-st.write('(printout t "Best plant is ivy!" crlf)')
+# knowledge base
+env.build('(deftemplate result (slot name))')
+# add facts to working memory
+env.assert_string(f'(result (name "{name}"))')
+# inference
+env.run()
+
+# output
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result':
+        results.append(fact['name']) #Why assert the fact? 
+        
+st.write(results[0],"Output")
