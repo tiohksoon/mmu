68b0b2b31e8312b92061b98839c03c0e5b66b2d7
tiohksoon <tiohksoon@gmail.com>
2025-11-10 04:48:26 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-#
+import streamlit as st
+
+st.title("My First Streamlit App")
+st.header("This is a Header")
+st.subheader("This is a Subheader")
+st.write("This is a paragraph of text using st.write().")
+st.text("This is plain text using st.text().")
